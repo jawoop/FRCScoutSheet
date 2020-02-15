64a67cca0da94c7a99f608a304fc84a83a16b6b5
jawoop <jake.postema@gmail.com>
2020-02-15 11:57:16 -0500

    collecting matches

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import requests
+import json
 from os.path import isfile
 
 assert isfile('headerfile'), 'This program requires a header file in the same directory called headerfile.'
 
+baseUrl = 'https://www.thebluealliance.com/api/v3'
+teamNum = 1405
+year = 2019
+eventName = 'Finger Lakes Regional'
+
 with open('headerfile') as headerfile:
     header = headerfile.readlines()[0]
 
-print(requests.get('https://www.thebluealliance.com/api/v3/status',
-                   headers={'X-TBA-Auth-Key': header}))
+events = requests.get(f'{baseUrl}/events/{year}/simple',
+                      headers={'X-TBA-Auth-Key': header,
+                               'year': str(year)})
+for event in events.json():
+    if event['name'] == eventName:
+        break
+assert event['name'] == eventName, f'No such event as {eventName}'
+
+matches = requests.get(f'{baseUrl}/event/{event["key"]}/matches/simple',
+                       headers={'X-TBA-Auth-Key': header,
+                                'event_key': event['key']})
+
+print(matches.json())
