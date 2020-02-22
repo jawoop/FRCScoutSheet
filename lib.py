6f7c176fd22a6c8aa69f7c748338934cc9f74a64
jawoop <jake.postema@gmail.com>
2020-02-22 15:50:02 -0500

    dynamic list in progress, time tracking

--- lib.py
+++ lib.py
@@ -1,12 +1,5 @@
 def sortBy(listOfDicts, key):
-    if '/' in key:
-        if key.count('/') > 1:
-            raise Exception("I don't know how to deal with this level of nested keys, please raise an issue on GitHub!")
-        else:
-            keysIndices = {dictionary[key.split('/')[0]][key.split('/')[1]]: listOfDicts.index(dictionary)
-                           for dictionary in listOfDicts}
-    else:
-        keysIndices = {dictionary[key]: listOfDicts.index(dictionary) for dictionary in listOfDicts}
+    keysIndices = {dictionary[key]: listOfDicts.index(dictionary) for dictionary in listOfDicts}
     sortedKeys = sorted(list(keysIndices.keys()))
     return [listOfDicts[keysIndices[k]] for k in sortedKeys]
 
