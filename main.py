b1f8c6120a7e1655b05183a9eebb11f1853b0992
jawoop <jake.postema@gmail.com>
2020-03-03 11:08:20 -0500

    unnecessary access improvements 3

--- main.py
+++ main.py
@@ -49,10 +49,12 @@ events = requests.get(f'{baseUrl}/events/{year}/simple',
 print([event['name'] for event in events.json()])
 for event in events.json():
     if event['name'] == eventName: break
-assert event[
-           'name'] == eventName, f"No such event as {eventName} in the following list of events: {sorted([event['name'] for event in events.json()])}"
+assert event['name'] == eventName, f"No such event as {eventName} in the following list of events: " \
+                                   f"{sorted([event['name'] for event in events.json()])}"
 print(f"Found our event: event['key'] = {event['key']}")
 
+update = {'basicFormatting': True, 'customList': True, 'matchData': True, 'matchDataUploaded': True, 'rankings': True,
+          'winners': True}
 dataUploaded = True
 needFormattingUpdates = True
 needMatchDataUpdates = True
@@ -86,14 +88,14 @@ while True:
     cycleStart = now()
 
     try:
-        if sheet.acell('L2').value and not disregardForceUpdate: forceAllUpdate = True
+        if sheet.acell('L2').value and not disregardForceUpdate: update = {k: True for (k, v) in update}
     except gspread.exceptions.APIError:
         print('Force update could not be accessed, please wait until the next cycle')
 
     try:
         if doNotTrackList != sheet.col_values(8)[1:] or pickList1 != sheet.col_values(9)[
                                                                      1:] or pickList2 != sheet.col_values(10)[1:]:
-            needCustomListUpdates = True
+            update['customList'] = True
         doNotTrackList = filterEntropy(sheet.col_values(8)[1:])
         pickList1 = filterEntropy(sheet.col_values(9)[1:])
         pickList2 = filterEntropy(sheet.col_values(10)[1:])
@@ -106,14 +108,14 @@ while True:
                                                  'event_key': event['key']}).json()
 
     if matchesReq.json() != mostRecentMatchesReq:
-        needRankingUpdates = True
+        update['rankings'] = True
         if checkAlliancesChanged(matchesReq, mostRecentMatchesReq):
-            needMatchDataUpdates = True
-            dataUploaded = False
+            update['matchData'] = True
+            update['matchDataUploaded'] = False
         if updatedMatchWinners(matchesReq, mostRecentMatchesReq):
-            needWinnerUpdates = True
+            update['winners'] = True
 
-    if needMatchDataUpdates or forceAllUpdate:
+    if update['matchData']:
         matchUpdateStart = now()
         matchesReq = requests.get(f"{baseUrl}/event/{event['key']}/matches/simple",
                                   headers={'X-TBA-Auth-Key': header,
@@ -154,7 +156,7 @@ while True:
         print(f'Updating match data took {matchUpdateEnd - matchUpdateStart}.')
 
     dataUploadStart = now()
-    while not dataUploaded:
+    while not update['matchDataUploaded']:
         try:
             print('Inserting data...')
             teamsListRange = sheet.range(f'A1:F{len(matches)}')
@@ -176,141 +178,140 @@ while True:
             sheet.update_cells(teamsListRange)
             sheet.update_cells(indicatorRange)
             print('Upload complete. Please check to make sure information was successfully uploaded.')
-            dataUploaded = True
+            update['matchDataUploaded'] = True
         except gspread.exceptions.APIError:
-            print(
-                f'Read requests overflow -- waiting 100 seconds (Waiting until {now() + datetime.timedelta(seconds=100)} to continue)')
+            print(f'Read requests overflow -- waiting 100 seconds (Waiting until '
+                  f'{now() + datetime.timedelta(seconds=100)} to continue)')
             time.sleep(100)
     dataUploadEnd = now()
     print(f'Uploading match data took {dataUploadEnd - dataUploadStart}.')
 
     cellsNeedingFormatting = []
-    if needFormattingUpdates or forceAllUpdate:
-        formattingUpdateStart = now()
-        try:
-            if forceAllUpdate:
-                format_cell_range(sheet, f'A1:C{len(matches)}', CellFormat(
-                    backgroundColor=Color(1, 0.9, 0.9),
-                    textFormat=TextFormat(bold=False, italic=False, underline=False, foregroundColor=Color(0.5, 0, 0)),
-                    horizontalAlignment='CENTER'
-                ))
-                format_cell_range(sheet, f'D1:F{len(matches)}', CellFormat(
-                    backgroundColor=Color(0.9, 0.9, 1),
-                    textFormat=TextFormat(bold=False, italic=False, underline=False, foregroundColor=Color(0, 0, 0.5)),
-                    horizontalAlignment='CENTER'
-                ))
+    formattingUpdateStart = now()
+    try:
+        if update['basicFormatting']:
+            format_cell_range(sheet, f'A1:C{len(matches)}', CellFormat(
+                backgroundColor=Color(1, 0.9, 0.9),
+                textFormat=TextFormat(bold=False, italic=False, underline=False, foregroundColor=Color(0.5, 0, 0)),
+                horizontalAlignment='CENTER'
+            ))
+            format_cell_range(sheet, f'D1:F{len(matches)}', CellFormat(
+                backgroundColor=Color(0.9, 0.9, 1),
+                textFormat=TextFormat(bold=False, italic=False, underline=False, foregroundColor=Color(0, 0, 0.5)),
+                horizontalAlignment='CENTER'
+            ))
 
-                print('Formatting special indicator cells')
-                format_cell_range(sheet, 'H1', CellFormat(
-                    backgroundColor=Color(0.5, 0.5, 0.5)
-                ))
-                format_cell_range(sheet, 'I1', CellFormat(
-                    backgroundColor=Color(0.29, 0.89, 0.45)
-                ))
-                format_cell_range(sheet, 'J1', CellFormat(
-                    backgroundColor=Color(0.76, 0.48, 0.63),
-                ))
-                format_cell_range(sheet, 'L1', CellFormat(
-                    backgroundColor=Color(1, 0, 0),
-                    textFormat=TextFormat(bold=True, foregroundColor=Color(0, 0, 1)),
-                    horizontalAlignment='CENTER'
-                ))
+            print('Formatting special indicator cells')
+            format_cell_range(sheet, 'H1', CellFormat(
+                backgroundColor=Color(0.5, 0.5, 0.5)
+            ))
+            format_cell_range(sheet, 'I1', CellFormat(
+                backgroundColor=Color(0.29, 0.89, 0.45)
+            ))
+            format_cell_range(sheet, 'J1', CellFormat(
+                backgroundColor=Color(0.76, 0.48, 0.63),
+            ))
+            format_cell_range(sheet, 'L1', CellFormat(
+                backgroundColor=Color(1, 0, 0),
+                textFormat=TextFormat(bold=True, foregroundColor=Color(0, 0, 1)),
+                horizontalAlignment='CENTER'
+            ))
 
-            if needMatchDataUpdates or forceAllUpdate:
-                print('Highlighting us...')
-                for cell in sheet.findall('1405'):
+        if update['matchData']:
+            print('Highlighting us...')
+            for cell in sheet.findall('1405'):
+                cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
+                                               CellFormat(
+                                                   backgroundColor=Color(1, 1, 0.75),
+                                                   textFormat=TextFormat(underline=True)
+                                               )))
+
+            for opponent in opponents:
+                print(f'Highlighting opponent {opponent[3:]}')
+                for cell in sheet.findall(opponent[3:]):
                     cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
                                                    CellFormat(
-                                                       backgroundColor=Color(1, 1, 0.75),
-                                                       textFormat=TextFormat(underline=True)
+                                                       textFormat=TextFormat(bold=True)
                                                    )))
 
-                for opponent in opponents:
-                    print(f'Highlighting opponent {opponent[3:]}')
-                    for cell in sheet.findall(opponent[3:]):
-                        cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
-                                                       CellFormat(
-                                                           textFormat=TextFormat(bold=True)
-                                                       )))
+            for partner in partners:
+                print(f'Highlighting partner {partner[3:]}')
+                for cell in sheet.findall(partner[3:]):
+                    cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
+                                                   CellFormat(
+                                                       textFormat=TextFormat(italic=True)
+                                                   )))
+        if update['rankings']:
+            for ranking in rankings:
+                print(f"Ranking team {ranking['team_key'][3:]} -- ranked {ranking['rank']}")
+                for cell in sheet.findall(ranking['team_key'][3:]):
+                    if rankings.index(ranking) < len(rankings) * 0.25:
+                        if ranking['team_key'] == ourTeamKey:
+                            cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
+                                                           CellFormat(
+                                                               textFormat=TextFormat(
+                                                                   foregroundColor=Color(0.38, 0.38, 0.13))
+                                                           )))
+                        else:
+                            cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
+                                                           CellFormat(
+                                                               textFormat=TextFormat(
+                                                                   foregroundColor=Color(0.38, 0.13, 0.13))
+                                                           ) if cell.col <= 3 else CellFormat(
+                                                               textFormat=TextFormat(
+                                                                   foregroundColor=Color(0.13, 0.13, 0.38))
+                                                           )))
 
-                for partner in partners:
-                    print(f'Highlighting partner {partner[3:]}')
-                    for cell in sheet.findall(partner[3:]):
-                        cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
-                                                       CellFormat(
-                                                           textFormat=TextFormat(italic=True)
-                                                       )))
-            if needRankingUpdates or forceAllUpdate:
-                for ranking in rankings:
-                    print(f"Ranking team {ranking['team_key'][3:]} -- ranked {ranking['rank']}")
-                    for cell in sheet.findall(ranking['team_key'][3:]):
-                        if rankings.index(ranking) < len(rankings) * 0.25:
-                            if ranking['team_key'] == ourTeamKey:
-                                cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
-                                                               CellFormat(
-                                                                   textFormat=TextFormat(
-                                                                       foregroundColor=Color(0.38, 0.38, 0.13))
-                                                               )))
-                            else:
-                                cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
-                                                               CellFormat(
-                                                                   textFormat=TextFormat(
-                                                                       foregroundColor=Color(0.38, 0.13, 0.13))
-                                                               ) if cell.col <= 3 else CellFormat(
-                                                                   textFormat=TextFormat(
-                                                                       foregroundColor=Color(0.13, 0.13, 0.38))
-                                                               )))
-
-                        elif rankings.index(ranking) >= len(rankings) * 0.75:
-                            if ranking['team_key'] == ourTeamKey:
-                                cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
-                                                               CellFormat(
-                                                                   textFormat=TextFormat(
-                                                                       foregroundColor=Color(0.63, 0.63, 0.38))
-                                                               )))
-                            else:
-                                cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
-                                                               CellFormat(
-                                                                   textFormat=TextFormat(
-                                                                       foregroundColor=Color(0.63, 0.38, 0.38))
-                                                               ) if cell.col <= 3 else CellFormat(
-                                                                   textFormat=TextFormat(
-                                                                       foregroundColor=Color(0.38, 0.38, 0.63))
-                                                               )))
-            if needWinnerUpdates or forceAllUpdate:
-                for match in matches:
-                    if match['winning_alliance']:
-                        print(f"Highlighting winner of match {match['match_number']}")
+                    elif rankings.index(ranking) >= len(rankings) * 0.75:
+                        if ranking['team_key'] == ourTeamKey:
+                            cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
+                                                           CellFormat(
+                                                               textFormat=TextFormat(
+                                                                   foregroundColor=Color(0.63, 0.63, 0.38))
+                                                           )))
+                        else:
+                            cellsNeedingFormatting.append((gspread.utils.rowcol_to_a1(cell.row, cell.col),
+                                                           CellFormat(
+                                                               textFormat=TextFormat(
+                                                                   foregroundColor=Color(0.63, 0.38, 0.38))
+                                                           ) if cell.col <= 3 else CellFormat(
+                                                               textFormat=TextFormat(
+                                                                   foregroundColor=Color(0.38, 0.38, 0.63))
+                                                           )))
+        if update['winners']:
+            for match in matches:
+                if match['winning_alliance']:
+                    print(f"Highlighting winner of match {match['match_number']}")
+                    cellsNeedingFormatting.append((
+                        f"{gspread.utils.rowcol_to_a1(matches.index(match) + 1, 1 if match['winning_alliance'] == 'red' else 4)}:"
+                        f"{gspread.utils.rowcol_to_a1(matches.index(match) + 1, 3 if match['winning_alliance'] == 'red' else 6)}",
+                        CellFormat(
+                            backgroundColor=Color(1, 0.75, 0.75)
+                        ) if match['winning_alliance'] == 'red' else CellFormat(
+                            backgroundColor=Color(0.75, 0.75, 1)
+                        )))
+                    if ourTeamKey in match['alliances'][match['winning_alliance']]['team_keys']:
                         cellsNeedingFormatting.append((
-                            f"{gspread.utils.rowcol_to_a1(matches.index(match) + 1, 1 if match['winning_alliance'] == 'red' else 4)}:"
-                            f"{gspread.utils.rowcol_to_a1(matches.index(match) + 1, 3 if match['winning_alliance'] == 'red' else 6)}",
+                            gspread.utils.rowcol_to_a1(matches.index(match) + 1,
+                                                       match['alliances'][match['winning_alliance']][
+                                                           'team_keys'].index(
+                                                           ourTeamKey)
+                                                       + (1 if ourTeamKey in match['alliances']['red'][
+                                                           'team_keys'] else 4)),
                             CellFormat(
-                                backgroundColor=Color(1, 0.75, 0.75)
-                            ) if match['winning_alliance'] == 'red' else CellFormat(
-                                backgroundColor=Color(0.75, 0.75, 1)
+                                textFormat=TextFormat(bold=True),
+                                backgroundColor=Color(1, 1, 0.5)
                             )))
-                        if ourTeamKey in match['alliances'][match['winning_alliance']]['team_keys']:
-                            cellsNeedingFormatting.append((
-                                gspread.utils.rowcol_to_a1(matches.index(match) + 1,
-                                                           match['alliances'][match['winning_alliance']][
-                                                               'team_keys'].index(
-                                                               ourTeamKey)
-                                                           + (1 if ourTeamKey in match['alliances']['red'][
-                                                               'team_keys'] else 4)),
-                                CellFormat(
-                                    textFormat=TextFormat(bold=True),
-                                    backgroundColor=Color(1, 1, 0.5)
-                                )))
-                    else:
-                        print(f"Match #{match['match_number']} has not resolved yet; no winner")
+                else:
+                    print(f"Match #{match['match_number']} has not resolved yet; no winner")
 
-            needMatchDataUpdates = False
+        update['matchData'] = False
 
-            print('Congratulations, complete formatting update!')
-            needFormattingUpdates = False
-        except gspread.exceptions.APIError:
-            print('Read requests overload: Waiting until cycle to update highlighting')
-            needFormattingUpdates = True
+        print('Congratulations, complete formatting update!')
+        needFormattingUpdates = False
+    except gspread.exceptions.APIError:
+        print('Read requests overload: Waiting until cycle to update highlighting')
+        needFormattingUpdates = True
 
         if needCustomListUpdates:
             for match in matches:
